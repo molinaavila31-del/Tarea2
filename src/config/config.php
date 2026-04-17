1fa5e657345aa6fe53a3608442833403ee17d57d
Avila <molinaavila31-del>
2026-04-17 17:00:28 -0300

    Un paso atras

--- src/config/config.php
+++ src/config/config.php
@@ -1,10 +1,9 @@
 <?php
-
-define('URL_PREFIX', '/src');
-define('URL_BASE_PATH', 'http://localhost:8080' . $_SERVER['HTTP_HOST'] . URL_PREFIX);
-define('DB_HOST', 'db');
-define('DB_PORT', '3306');  // Puerto estándar de MySQL, cambia según necesidades
-define('DB_NAME', 'mvc_db');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_CHARSET', 'utf8mb4');
+//define('URL_PREFIX', '/');
+//define('URL_BASE_PATH', 'http://localhost:8080' . $_SERVER['HTTP_HOST'] . URL_PREFIX);
+//define('DB_HOST', 'db');
+//define('DB_PORT', '3306');  // Puerto estándar de MySQL, cambia según necesidades
+//define('DB_NAME', 'mvc_db');
+//define('DB_USER', 'root');
+//define('DB_PASSWORD', 'root');
+//define('DB_CHARSET', 'utf8mb4');
